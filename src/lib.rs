1b25746ee392721012a3b5c664ee53425dbcb788
Markus Kohlhase <markus.kohlhase@slowtec.de>
2019-12-23 18:36:03 +0100

    Format code with rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -24,84 +24,64 @@ use std::f64::consts::PI;
 
 // date/time constants and conversions
 
-const MILLISECONDS_PER_DAY  : u32 = 1000 * 60 * 60 * 24;
-const J1970                 : u32 = 2440588;
-const J2000                 : u32 = 2451545;
-const TO_RAD                : f64 = PI / 180.0;
-const OBLIQUITY_OF_EARTH    : f64 = 23.4397  * TO_RAD;
-const PERIHELION_OF_EARTH   : f64 = 102.9372 * TO_RAD;
+const MILLISECONDS_PER_DAY: u32 = 1000 * 60 * 60 * 24;
+const J1970: u32 = 2440588;
+const J2000: u32 = 2451545;
+const TO_RAD: f64 = PI / 180.0;
+const OBLIQUITY_OF_EARTH: f64 = 23.4397 * TO_RAD;
+const PERIHELION_OF_EARTH: f64 = 102.9372 * TO_RAD;
 
 /// Holds the [azimuth](https://en.wikipedia.org/wiki/Azimuth)
 /// and [altitude](https://en.wikipedia.org/wiki/Horizontal_coordinate_system)
 /// angles of the sun position.
 #[derive(Debug, Clone)]
 pub struct Position {
-  pub azimuth   : f64,
-  pub altitude  : f64
+    pub azimuth: f64,
+    pub altitude: f64,
 }
 
 fn to_julian(unixtime_in_ms: i64) -> f64 {
-  unixtime_in_ms as f64 /
-  (MILLISECONDS_PER_DAY as f64) - 0.5 + J1970 as f64
+    unixtime_in_ms as f64 / (MILLISECONDS_PER_DAY as f64) - 0.5 + J1970 as f64
 }
 
 fn to_days(unixtime_in_ms: i64) -> f64 {
-  to_julian(unixtime_in_ms) - J2000 as f64
+    to_julian(unixtime_in_ms) - J2000 as f64
 }
 
 // general calculations for position
 
-fn right_ascension(l:f64, b:f64) -> f64 {
-  (
-    l.sin() * OBLIQUITY_OF_EARTH.cos() -
-    b.tan() * OBLIQUITY_OF_EARTH.sin()
-  )
-  .atan2(l.cos())
+fn right_ascension(l: f64, b: f64) -> f64 {
+    (l.sin() * OBLIQUITY_OF_EARTH.cos() - b.tan() * OBLIQUITY_OF_EARTH.sin()).atan2(l.cos())
 }
 
-fn declination(l:f64, b:f64) -> f64 {
-  (
-    b.sin() * OBLIQUITY_OF_EARTH.cos() +
-    b.cos() * OBLIQUITY_OF_EARTH.sin() * l.sin()
-  )
-  .asin()
+fn declination(l: f64, b: f64) -> f64 {
+    (b.sin() * OBLIQUITY_OF_EARTH.cos() + b.cos() * OBLIQUITY_OF_EARTH.sin() * l.sin()).asin()
 }
 
-fn azimuth(h:f64, phi:f64, dec:f64) -> f64  {
-  h.sin()
-  .atan2(
-    h.cos()   * phi.sin() -
-    dec.tan() * phi.cos()
-  ) + PI
+fn azimuth(h: f64, phi: f64, dec: f64) -> f64 {
+    h.sin().atan2(h.cos() * phi.sin() - dec.tan() * phi.cos()) + PI
 }
 
-fn altitude(h:f64, phi:f64, dec:f64) -> f64 {
-  (
-    phi.sin() * dec.sin() +
-    phi.cos() * dec.cos() * h.cos()
-  )
-  .asin()
+fn altitude(h: f64, phi: f64, dec: f64) -> f64 {
+    (phi.sin() * dec.sin() + phi.cos() * dec.cos() * h.cos()).asin()
 }
 
-fn sidereal_time(d:f64, lw:f64) -> f64 {
-  (280.16 + 360.9856235 * d).to_radians() - lw
+fn sidereal_time(d: f64, lw: f64) -> f64 {
+    (280.16 + 360.9856235 * d).to_radians() - lw
 }
 
 // general sun calculations
 
-fn solar_mean_anomaly(d:f64) -> f64 {
-  (357.5291 + 0.98560028 * d).to_radians()
+fn solar_mean_anomaly(d: f64) -> f64 {
+    (357.5291 + 0.98560028 * d).to_radians()
 }
 
-fn equation_of_center(m:f64) -> f64 {
-  (1.9148 * (1.0 * m).sin() +
-   0.02   * (2.0 * m).sin() +
-   0.0003 * (3.0 * m).sin()
-  ).to_radians()
+fn equation_of_center(m: f64) -> f64 {
+    (1.9148 * (1.0 * m).sin() + 0.02 * (2.0 * m).sin() + 0.0003 * (3.0 * m).sin()).to_radians()
 }
 
-fn ecliptic_longitude(m:f64) -> f64 {
-  m + equation_of_center(m) + PERIHELION_OF_EARTH + PI
+fn ecliptic_longitude(m: f64) -> f64 {
+    m + equation_of_center(m) + PERIHELION_OF_EARTH + PI
 }
 
 /// Calculates the sun position for a given date and latitude/longitude.
@@ -112,20 +92,19 @@ fn ecliptic_longitude(m:f64) -> f64 {
 /// * `lon`       - [longitude](https://en.wikipedia.org/wiki/Longitude) in degrees.
 /// calculates the sun position for a given date and latitude/longitude
 pub fn pos(unixtime_in_ms: i64, lat: f64, lon: f64) -> Position {
+    let lw = -lon.to_radians();
+    let phi = lat.to_radians();
+    let d = to_days(unixtime_in_ms);
+    let m = solar_mean_anomaly(d);
+    let l = ecliptic_longitude(m);
+    let dec = declination(l, 0.0);
+    let ra = right_ascension(l, 0.0);
+    let h = sidereal_time(d, lw) - ra;
 
-  let lw  = -lon.to_radians();
-  let phi = lat.to_radians();
-  let d   = to_days(unixtime_in_ms);
-  let m   = solar_mean_anomaly(d);
-  let l   = ecliptic_longitude(m);
-  let dec = declination(l, 0.0);
-  let ra  = right_ascension(l, 0.0);
-  let h   = sidereal_time(d, lw) - ra;
-
-  Position {
-    azimuth  :  azimuth(h, phi, dec),
-    altitude : altitude(h, phi, dec)
-  }
+    Position {
+        azimuth: azimuth(h, phi, dec),
+        altitude: altitude(h, phi, dec),
+    }
 }
 
 #[cfg(test)]
